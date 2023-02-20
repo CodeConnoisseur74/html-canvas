c1274110777592b39846bb780853f3e5b6be54f9
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-02-20 21:13:28 +0100

    add init and animate functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ const mouse = {
     x: undefined,
     y: undefined,
 }
+
 canvas.addEventListener('click', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
@@ -25,8 +26,10 @@ canvas.addEventListener('mousemove', function (event) {
 
 class Particle {
     constructor() {
-        this.x = mouse.x;
-        this.y = mouse.y;
+        //this.x = mouse.x;
+        //this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.size = Math.random() * 5 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
@@ -44,13 +47,23 @@ class Particle {
 }
 
 function init() {
-    for (let i = 0; i < 100; i++){
+    for (let i = 0; i < 100; i++) {
         particlesArray.push(new Particle());
+    }
+}
+init();
+
+
+function handleParticles() {
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
     }
 }
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
     requestAnimationFrame(animate);
 }
 animate();
